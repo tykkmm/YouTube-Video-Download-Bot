d47ef3dfcee863359b110dd5b6780f891f6733dc
tykkmm <tykhanew45@gmail.com>
2024-07-21 11:43:14 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,69 +1,92 @@
 
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL
-from pyrogram import Client, filters
-from pytube import YouTube
-import asyncio
 
-# Replace 'YOUR_API_ID', 'YOUR_API_HASH', and 'YOUR_BOT_TOKEN' with your actual values
+class Config(object):
+    LOGGER = True
 
-API_ID = '27952989'
-API_HASH = '74f04808a359e9a516e955ec243613ca'
-BOT_TOKEN = '6917606251:AAH_wYnS-okZSzGAQ5XKddAair89G4aHdWo'
+    #####
 
-# Create a Pyrogram client
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+    ANILIST_CLIENT = getenv("ANILIST_CLIENT", "8679")
+  
+    ANILIST_SECRET = getenv("ANILIST_SECRET", "NeCEq9A1hVnjsjZlTZyNvqK11krQ4HtSliaM7rTN")
+  
+    API_ID = getenv("API_ID",
+"29426486")
+   
+    API_HASH = getenv("API_HASH",
+"d71ad4ec048ab41677a1a439b21ff0c9")
+   
+    TOKEN = getenv("TOKEN",
+"7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To")
+  
+    OWNER_ID = getenv("OWNER_ID", "5976437467") 
 
-# Start command handler
-@app.on_message(filters.command("start"))
-def start(client, message):
-    user = message.from_user
-    message.reply_text(f"Hello, @{user.username}!\n\nSend me the YouTube link of the video you want to upload...Bot Owner @sccwaleyarr ")
-
-# Help command handler
-@app.on_message(filters.command("help"))
-def help(client, message):
-    help_text = """
-    Welcome to the YouTube Video Uploader Bot!
-
-To upload a YouTube video, simply send me the YouTube link.
+    OWNER_USERNAME = ("OWNER_USERNAME", "TY3052432")
     
-Enjoy using the bot!
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "the_friendz")
+   
+    START_IMG = getenv("START_IMG", "https://graph.org/file/eaa3a2602e43844a488a5.jpg")
 
-   ©️ Channel : @rajcourse
-    """
-    message.reply_text(help_text)
+    JOIN_LOGGER = getenv("JOIN_LOGGER", "-1002244796953")
+   
+    EVENT_LOGS = getenv("EVENT_LOGS",  "-1002244796953")
+  
+    ERROR_LOGS = getenv("ERROR_LOGS", "-1002244796953")
 
-# Message handler for processing YouTube links
-@app.on_message(filters.regex(r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'))
-async def process_youtube_link(client, message):
-    youtube_link = message.text
-    try:
-        # Downloading text message
-        downloading_msg = await message.reply_text("Downloading video🚦 @sccwaleyarr ...")
+    MONGO_DB_URI= getenv("MONGO_DB_URI", "mongodb+srv://Arainanwar009:Ux955XkbkTntynx@cluster0.b1chu9a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+   
+    LOG_CHANNEL = getenv("LOG_CHANNEL", "-1002244796953")
+   
+    BOT_USERNAME = getenv("BOT_USERNAME" , "Itxmety73_bot")
+   
+    DATABASE_URL = getenv("DATABASE_URL", None)
 
-        # Download the YouTube video
-        yt = YouTube(youtube_link)
-        video = yt.streams.filter(progressive=True, file_extension='mp4').first()
-        video.download(filename='downloaded_video.mp4')
+    CASH_API_KEY = getenv("CASH_API_KEY", "V48U2FLLKRHSVD4X")
+    
+    TIME_API_KEY = getenv("TIME_API_KEY", "1CUBX1HXGNHW")
 
-        # Uploading text message
-        uploading_msg = await message.reply_text("Uploading video...")
+    SPAMWATCH_API = getenv("SPAMWATCH_API", "3624487efd8e4ca9c949f1ab99654ad1e4de854f41a14afd00f3ca82d808dc8c")
+    
+    SPAMWATCH_SUPPORT_CHAT = getenv("SPAMWATCH_SUPPORT_CHAT", "h_cc_help")
+    
+    WALL_API = getenv("WALL_API", "2455acab48f3a935a8e703e54e26d121")
+    
+    REM_BG_API_KEY = getenv("REM_BG_API_KEY", "xYCR1ZyK3ZsofjH7Y6hPcyzC")
+    
+    OPENWEATHERMAP_ID = getenv("OPENWEATHERMAP_ID", "887da2c60d9f13fe78b0f9d0c5cbaade")
 
-        # Send the video file to the user
-        sent_message = await app.send_video(message.chat.id, video=open('downloaded_video.mp4', 'rb'), caption=yt.title)
+    BAN_STICKER = getenv("BAN_STICKER", "CAACAgEAAxkBAAIrTWYljyX_lqcubkAzg0jy45CRvxAFAAKvAgACrLHoRU50VVvh3xWwNAQ")
 
-        # Delay for a few seconds and delete downloading and uploading
-        await asyncio.sleep(2)
-        await downloading_msg.delete()
-        await uploading_msg.delete()
+    HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-        # Send successful upload message
-        await message.reply_text("\n\nOWNER : @rajcourse 🥀\n\nSUCCESSFULLY UPLOADED!")
+    HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
-    except Exception as e:
-        error_text = 'Error: Failed to process the YouTube link. Please make sure the link is valid and try again.'
-        await message.reply_text(error_text)
+    LASTFM_API_KEY = getenv("LASTFM_API_KEY", "8f3315b5806c21004b2822f07825187d")
+    
+    # Optional fields
+    
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    DRAGONS = []  # User id of sudo users
+    DEV_USERS = []  # User id of dev users
+    DEMONS = []  # User id of support users
+    TIGERS = []  # User id of tiger users
+    WOLVES = []  # User id of whitelist users
 
-# Start the bot
-print("🎊 I AM ALIVE 🎊")
-app.run()
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
+    DEL_CMDS = True
+    INFOPIC = True
+    LOAD = []
+    NO_LOAD = []
+    STRICT_GBAN = True
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    WORKERS = 8
+    
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
+
+  
